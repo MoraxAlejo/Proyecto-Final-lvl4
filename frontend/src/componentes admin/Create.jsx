648163cd2803f5f57aebdcd5f3a5dc0b7578d709
MoraxAlejo <alejodeviluke@gmail.com>
2023-10-09 17:50:31 -0500

    CRUD USUARIOS COMPLETO y Bitacoras Casi

--- frontend/src/componentes admin/Create.jsx
+++ frontend/src/componentes admin/Create.jsx
@@ -1,10 +1,13 @@
+import {  useState } from "react";
+import "../css/details.css"
+import Nav from "./Nav"
 import axios from "axios";
-import { useState, useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+ const endpoint = 'http://127.0.0.1:8000/api/usuarios';
 
-const endpoint = 'http://127.0.0.1:8000/api/usuarios/';
-
-export const EditUsuario = () => {
+export default function Parametros() {
+   
+   
     const [usuario, setUsuario] = useState('');
     const [clave, setClave] = useState('');
     const [idrol, setIdrol] = useState(0);
@@ -15,16 +18,11 @@ export const EditUsuario = () => {
     const [usuariocreacion, setUsuariocreacion] = useState('');
     const [usuariomodificacion, setUsuariomodificacion] = useState('');
     const [idpersona, setIdpersona] = useState('');
-    const [editSuccess, setEditSuccess] = useState(false);
-    const [editError, setEditError] = useState(false);
     const navigate = useNavigate();
-    const { idusuario } = useParams();
-
-    const update = async (e) => {
+    const create = async (e) => {
         e.preventDefault();
 
-        try {
-            await axios.put(`${endpoint}${idusuario}`, {
+            await axios.post(`${endpoint}`, {
                 idpersona: idpersona,
                 usuario: usuario,
                 clave: clave,
@@ -37,85 +35,56 @@ export const EditUsuario = () => {
                 usuariomodificacion: usuariomodificacion
             });
 
-            setEditSuccess(true);
-            setEditError(false);
+            
 
             navigate("/usuarios");
-        } catch (error) {
-            console.error(error);
-            setEditSuccess(false);
-            setEditError(error.response?.data?.error || 'Error al editar los datos.');
-        }
-    }
 
-
-    useEffect(() => {
-        const getUsuarioById = async () => {
-            const response = await axios.get(`${endpoint}${idusuario}`);
-            setUsuario(response.data.usuario);
-            setClave(response.data.clave);
-            setIdrol(response.data.idrol);
-            setHabilitado(response.data.habilitado);
-            setIdpersona(response.data.idpersona);
-            setFecha(response.data.fecha);
-            setFechamodificacion(response.data.fechamodificacion);
-            setUsuariomodificacion(response.data.usuariomodificacion);
-            setUsuariocreacion(response.data.usuariocreacion);
-            setFechacreacion(response.data.fechacreacion);
 
         }
-        getUsuarioById();
-    }, [idusuario]);
-
 
     return (
-        <div className="h-screen w-full flex justify-center items-center">
+        <div className="px-10 flex flex-col items-center gap-10 pb-10">
+            <Nav />
+            
             <main className="border border-gray-300 rounded-2xl w-[600px]">
                 <div className="flex py-5 justify-between px-8">
-                    <h2 className=" text-2xl font-normal">Edit Info</h2>
+                    <h2 className=" text-2xl font-normal">Create</h2>
                 </div>
                 <hr className="border border-gray-200 w-full" />
-                <form onSubmit={update} className="py-5 px-8">
+                <form onSubmit={create} className="py-5 px-8">
                     <div className="w-full flex justify-between">
                         <section className="flex flex-col justify-center ">
                             <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900">ID rol</label>
                             <input
-                                value={idrol}
-                                onChange={(e) => setIdrol(e.target.value)} // Asigna el valor a idrol
                                 type="number"
                                 placeholder=""
+                                onChange={(e) => setIdrol(e.target.value)}
                                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5"
                             />
 
-                        </section>
-
-                        <section className="flex flex-col justify-center ">
-                            <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900">ID usuario</label>
-                            <input
-                                value={idusuario} // Asigna el valor de idusuario al campo
-                                readOnly // Asegura que el campo sea de solo lectura
-                                type="number"
-                                placeholder=""
-                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5"
-                            />
                         </section>
 
                     </div>
+                    
                     <section className="flex flex-col justify-center">
                         <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900">Usuario</label>
-                        <input value={usuario} onChange={(e) => setUsuario(e.target.value)} type="text" placeholder="" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5" />
+                        <input type="text" 
+                        onChange={(e) => setUsuario(e.target.value)}
+                        placeholder="" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5" />
                     </section>
 
                     <section className="flex flex-col justify-center">
                         <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900">Clave</label>
-                        <input value={clave} onChange={(e) => setClave(e.target.value)} type="text" placeholder="" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5" />
+                        <input type="text" 
+                        onChange={(e) => setClave(e.target.value)}
+                        placeholder="" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5" />
                     </section>
+
                     <section className="flex flex-col justify-center">
                         <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900">Habilitado</label>
                         <input
-                            value={habilitado}
-                            onChange={(e) => setHabilitado(e.target.value)}
                             type="text"
+                            onChange={(e) => setHabilitado(e.target.value)}
                             placeholder=""
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5"
                         />
@@ -124,10 +93,9 @@ export const EditUsuario = () => {
                     <section className="flex flex-col justify-center">
                         <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900">Fecha</label>
                         <input
-                            value={fecha}
-                            onChange={(e) => setFecha(e.target.value)}
                             type="text"
                             placeholder=""
+                            onChange={(e) => setFecha(e.target.value)}
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5"
                         />
                     </section>
@@ -135,10 +103,9 @@ export const EditUsuario = () => {
                     <section className="flex flex-col justify-center">
                         <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900">Fecha Creación</label>
                         <input
-                            value={fechacreacion}
-                            onChange={(e) => setFechacreacion(e.target.value)}
                             type="text"
                             placeholder=""
+                            onChange={(e) => setFechacreacion(e.target.value)}
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5"
                         />
                     </section>
@@ -146,10 +113,9 @@ export const EditUsuario = () => {
                     <section className="flex flex-col justify-center">
                         <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900">Fecha Modificación</label>
                         <input
-                            value={fechamodificacion}
-                            onChange={(e) => setFechamodificacion(e.target.value)}
                             type="text"
                             placeholder=""
+                            onChange={(e) => setFechamodificacion(e.target.value)}
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5"
                         />
                     </section>
@@ -157,9 +123,8 @@ export const EditUsuario = () => {
                     <section className="flex flex-col justify-center">
                         <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900">Usuario Creación</label>
                         <input
-                            value={usuariocreacion}
-                            onChange={(e) => setUsuariocreacion(e.target.value)}
                             type="text"
+                            onChange={(e) => setUsuariocreacion(e.target.value)}
                             placeholder=""
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5"
                         />
@@ -168,9 +133,8 @@ export const EditUsuario = () => {
                     <section className="flex flex-col justify-center">
                         <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900">Usuario Modificación</label>
                         <input
-                            value={usuariomodificacion}
-                            onChange={(e) => setUsuariomodificacion(e.target.value)}
                             type="text"
+                            onChange={(e) => setUsuariomodificacion(e.target.value)}
                             placeholder=""
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5"
                         />
@@ -179,24 +143,19 @@ export const EditUsuario = () => {
                     <section className="flex flex-col justify-center">
                         <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900">ID Persona</label>
                         <input
-                            value={idpersona}
-                            onChange={(e) => setIdpersona(e.target.value)}
                             type="text"
                             placeholder=""
+                            onChange={(e) => setIdpersona(e.target.value)}
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5"
                         />
                     </section>
 
-                    <button className="bg-blue-500 hover:bg-blue-600 py-2 px-5 rounded-md text-white my-5" type="submit">Save</button>
+                    <button className="mt-5 bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-600 hover:to-blue-800 text-white font-bold py-3 px-6 rounded-full shadow-lg transform transition-all duration-500 ease-in-out hover:scale-110 hover:brightness-110 hover:animate-pulse active:animate-bounce">Create</button>
+
                 </form>
-                {editSuccess && (
-                    <div className="alert alert-success">Datos editados con éxito.</div>
-                )}
-                {editError && (
-                    <div className="alert alert-error">Error al editar los datos.</div>
-                )}
             </main>
         </div>
     )
-
 }
+
+
